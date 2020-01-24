a102463bf38c96cf889caef1059e3fb2e1a55ae0
kunwithgithub <kunhuang@ucdavis.edu>
2020-01-23 16:28:56 -0800

    updated for chp 7 codes

---
+++ Source Code for Exercises/SymbolEntry.java
@@ -0,0 +1,13 @@
+public class SymbolEntry extends Object{
+
+   private String name;
+
+   public SymbolEntry(String id){
+      name = id;
+   }
+
+   public String toString(){
+      return name;
+   }
+
+}
